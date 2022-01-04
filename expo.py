8de7f350c5a99e4c525ec953de936c3e554eb348
linda Bohorquez <lindavalentina0206@gmail.com>
2022-01-04 17:32:57 -0500

    feature: quiz

--- expo.py
+++ expo.py
@@ -1,24 +1,28 @@
 # PRIMER EJERCICIO
-# Comprobar si el usuario introduce un numero, si no es numero indicar que esta mal,
+# Comprobar si el usuario introduce un numero, si no es numero indicar que esta mal.
 # Si es numero, decir si es par o impar
 
 # Prueba
 texto = "hola"
 numero = "123"
+maria = 1
 print(texto.isnumeric())
 print(numero.isnumeric())
 
 # Resuleta la prueba seguiremos con el ejercicio
 # SOLUCION
-num = input("escribe un numero ")
+num = input("escribe un numero entero positivo ")
 if(not(num.isnumeric())):
     print("Datos incorrectos, no es un numero")
 else:
-    num = int(num)
-    if(num%2 == 0):
-        print("es par")
+    if(num == "0"):
+        print('es cero')
     else:
-        print("es impar")
+        num = int(num)
+        if(num%2 == 0):
+            print("es par")
+        else:
+            print("es impar")
 
 
 # SEGUNDO EJERCICIO 
